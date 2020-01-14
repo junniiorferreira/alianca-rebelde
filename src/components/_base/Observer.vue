d91fecb6c09057b8158d347610e11e6f06a3fe45
kyrvim <kyrvim@gmail.com>
2020-01-14 12:58:21 -0200

    refactory - II

---
+++ src/components/_base/Observer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="observer">
+    <p>Carregando...</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Observer',
+    data() {
+        return {
+            observer: null
+        }
+    },
+    mounted() {
+        this.observer = new IntersectionObserver(([entry]) => {
+            if (entry && entry.isIntersecting) {
+                this.$emit('intersect')
+            }
+        });
+        this.observer.observe(this.$el)
+    }
+}
+</script>
+
+<style>
+    .observer p{
+        padding: 30px;
+    }
+</style>
